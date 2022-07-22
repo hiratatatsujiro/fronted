5a1b415f352c58944460e284539bc5f01bfb3cec
平田達二郎 <hiratatoorujirou@hiratatoorujirounoMacBook-Pro.local>
2022-07-22 10:10:28 +0900

    vuex

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,16 @@
 <template>
-  <HelloWorld msg="HelloWorld" />
+  <CalendarPage />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import CalendarPage from "./components/CalendarPage.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    CalendarPage,
   },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/CalendarPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Calender</h1>
+    <ul>
+      <li v-for="event in events" :key="event.id">
+        {{ event.name }}
+      </li>
+    </ul>
+    <button type="submit" @click="fetchEvents()">fetchEvents</button>
+
+    <CalendarDetails />
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+import CalendarDetails from "./CalendarDetails.vue";
+export default {
+  name: "CalenderPage",
+  computed: {
+    ...mapGetters("events", ["events"]),
+  },
+  methods: {
+    ...mapActions("events", ["fetchEvents"]),
+  },
+  components: { CalendarDetails },
+};
+</script>
+
+<style></style>
